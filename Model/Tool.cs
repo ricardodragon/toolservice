9c80e42a40a47a32b1d67fbbca1c1f7626eb5f5c
Fernando Souza Silva <fernandos@spi.local>
2017-12-26 10:49:03 -0200

    Create model

---
+++ Model/Tool.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+namespace toolservice.Model
+{
+    public class Tool
+    {
+        public int id{get;set;}
+        [MaxLength(50)]
+        public string name{get;set;}
+        [MaxLength(100)]
+        public string description{get;set;}
+        [Required]
+        public double lifeCycle{get;set;}
+        public double currentLife{get;set;}
+        [Required]
+        public string unitOfMeasurement{get;set;}
+        [Required]
+        public ToolType type{get;set;}
+    }
+}
---
+++ Model/ToolType.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace toolservice.Model
+{
+    public class ToolType
+    {
+        public int id{get;set;}
+        [MaxLength(50)]
+        public string name{get;set;}
+        [MaxLength(100)]
+        public string description{get;set;}
+        [Column("thingGroupIds", TypeName = "integer[]")]
+        public int[] thingGroupIds{get;set;}
+
+    }
+}
