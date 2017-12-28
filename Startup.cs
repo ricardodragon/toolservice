54e63d33202c5f0772bef2bae9c4fb71409c8465
Fernando Souza Silva <fernandos@spi.local>
2017-12-28 09:18:35 -0200

    enabled cors

--- Startup.cs
+++ Startup.cs
@@ -46,12 +46,15 @@ namespace toolservice
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors("CorsPolicy");
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
             app.UseMvc();
+
         }
     }
 }
