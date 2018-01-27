2b5fdf9dce1b3ed8b0b66ca28c25935da34b87e6
Marcus Vinicius Bianchi dos Santos <marcuss@spi.local>
2018-01-27 12:03:24 -0200

    Changes for cloud production.

--- Program.cs
+++ Program.cs
@@ -8,18 +8,16 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace toolservice
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
+namespace toolservice {
+    public class Program {
+        public static void Main (string[] args) {
+            BuildWebHost (args).Run ();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+        public static IWebHost BuildWebHost (string[] args) =>
+            WebHost.CreateDefaultBuilder (args)1
+            .UseUrls ("http://*:5004")
+            .UseStartup<Startup> ()
+            .Build ();
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -4,75 +4,71 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using toolservice.Actions;
+using toolservice.Data;
 using toolservice.Service;
 using toolservice.Service.Interface;
-using toolservice.Data;
-using toolservice.Actions;
-using Newtonsoft.Json;
 
-namespace toolservice
-{
-    public class Startup
-    {
+namespace toolservice {
+    public class Startup {
 
-        public Startup(IConfiguration configuration)
-        {
+        public Startup (IConfiguration configuration) {
             this.Configuration = configuration;
 
         }
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddCors(o => o.AddPolicy("CorsPolicy", builder =>
-                {
-                    builder.AllowAnyOrigin()
-                           .AllowAnyMethod()
-                           .AllowAnyHeader();
-                }));
-            services.AddSingleton<IConfiguration>(Configuration);
-            services.AddSingleton<IThingService, ThingService>();
-            services.AddSingleton<IThingGroupService, ThingGroupService>();
-            services.AddTransient<IToolTypeService, ToolTypeService>();
-            services.AddTransient<IToolService, ToolService>();
-            services.AddTransient<IStateTransitionHistoryService, StateTransitionHistoryService>();
-            services.AddDbContext<ApplicationDbContext>(options =>
-               options.UseNpgsql(Configuration.GetConnectionString("ToolDB")));
-            services.AddTransient<IStateManagementService, StateManagementService>();
-            services.AddTransient<IStateManagementService, StateManagementService>();
-            services.AddTransient<IAssociateToolService, AssociateToolService>();
-            services.AddSingleton<IList<IPostStateChangeAction>>(sp =>
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddCors (o => o.AddPolicy ("CorsPolicy", builder => {
+                builder.AllowAnyOrigin ()
+                    .AllowAnyMethod ()
+                    .AllowAnyHeader ();
+            }));
+            services.AddSingleton<IConfiguration> (Configuration);
+            services.AddSingleton<IThingService, ThingService> ();
+            services.AddSingleton<IThingGroupService, ThingGroupService> ();
+            services.AddTransient<IToolTypeService, ToolTypeService> ();
+            services.AddTransient<IToolService, ToolService> ();
+            services.AddTransient<IStateTransitionHistoryService, StateTransitionHistoryService> ();
+            services.AddDbContext<ApplicationDbContext> (options =>
+                options.UseNpgsql (Configuration.GetConnectionString ("ToolDB")));
+            services.AddTransient<IStateManagementService, StateManagementService> ();
+            services.AddTransient<IStateManagementService, StateManagementService> ();
+            services.AddTransient<IAssociateToolService, AssociateToolService> ();
+            services.AddSingleton<IList<IPostStateChangeAction>> (sp =>
 
-                 new List<IPostStateChangeAction>{
+                new List<IPostStateChangeAction> {
 
-                    new TriggerAction(Configuration)
+                    new TriggerAction (Configuration)
 
                 }
             );
 
-            services.AddMvc().AddJsonOptions(options =>
-                {
-                    options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
-                }); ;
+            services.AddMvc ().AddJsonOptions (options => {
+                options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+            });;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            app.UseCors("CorsPolicy");
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            app.UseResponseCaching ();
+            app.UseCors ("CorsPolicy");
+            app.UseForwardedHeaders (new ForwardedHeadersOptions {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
             }
 
-            app.UseMvc();
+            app.UseMvc ();
 
         }
     }
