ec9c56e41656ea5e16e58b5fbf6e3bb8c66df7f7
Marcus Vinicius Bianchi dos Santos <marcuss@spi.local>
2018-03-28 14:22:16 -0300

    Correction on Model Miss

--- Model/ToolType.cs
+++ Model/ToolType.cs
@@ -1,20 +1,23 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-namespace toolservice.Model
-{
-    public class ToolType
-    {
+namespace toolservice.Model {
+    public class ToolType {
         public int toolTypeId { get; set; }
+
         [Required]
-        [MaxLength(50)]
+        [MaxLength (50)]
         public string name { get; set; }
-        [MaxLength(100)]
+
+        [MaxLength (100)]
         public string description { get; set; }
-        [Column("thingGroupIds", TypeName = "integer[]")]
+
+        [Column ("thingGroupIds", TypeName = "integer[]")]
         public int[] thingGroupIds { get; set; }
+
         [Required]
         public string status { get; set; }
+
         [NotMapped]
         public IList<ThingGroup> thingGroups { get; set; }
 
--- Service/AssociateToolService.cs
+++ Service/AssociateToolService.cs
@@ -1,98 +1,89 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using toolservice.Service.Interface;
-using toolservice.Data;
-using toolservice.Model;
 using System.Net;
-using Microsoft.Extensions.Configuration;
-using Newtonsoft.Json.Linq;
-using Newtonsoft.Json;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using toolservice.Data;
+using toolservice.Model;
+using toolservice.Service.Interface;
 
-namespace toolservice.Service
-{
-    public class AssociateToolService : IAssociateToolService
-    {
+namespace toolservice.Service {
+    public class AssociateToolService : IAssociateToolService {
         private readonly IToolService _toolService;
         private readonly IToolTypeService _toolTypeService;
         private readonly IStateManagementService _stateManagementService;
         private readonly IThingGroupService _thingGroupService;
         private readonly IConfiguration _configuration;
-        private readonly HttpClient client = new HttpClient();
+        private readonly HttpClient client = new HttpClient ();
 
-        public AssociateToolService(IStateManagementService stateManagementService,
+        public AssociateToolService (IStateManagementService stateManagementService,
             IToolTypeService toolTypeService,
             IToolService toolService,
             IThingGroupService thingGroupService,
-            IConfiguration configuration)
-        {
+            IConfiguration configuration) {
             _toolTypeService = toolTypeService;
             _toolService = toolService;
             _stateManagementService = stateManagementService;
             _thingGroupService = thingGroupService;
             _configuration = configuration;
         }
-        public async Task<(Tool, string)> AssociateTool(int thingId, int toolId)
-        {
-            var tool = await _toolService.getTool(toolId);
+        public async Task<(Tool, string)> AssociateTool (int thingId, int toolId) {
+            var tool = await _toolService.getTool (toolId);
             if (tool == null)
                 return (null, "Tool Not Found");
-            var availableTools = await _toolService.getToolsAvailable();
-            if (availableTools.Where(x=>x.toolId == toolId).Count()<1)
+            var availableTools = await _toolService.getToolsAvailable ();
+            if (availableTools.Where (x => x.toolId == toolId).Count () < 1)
                 return (null, "Tool Not Available");
-            var toolType = await _toolTypeService.getToolType(tool.typeId.Value);
+            var toolType = await _toolTypeService.getToolType (tool.typeId.Value);
             if (toolType == null)
                 return (null, "Tool Type Not Found");
             var thingGroups = toolType.thingGroups;
             bool contains = false;
-            foreach (var group in thingGroups)
-            {
-                var (completeGroup, status) = await _thingGroupService.getGroup(group.thingGroupId);
+            foreach (var group in thingGroups) {
+                var (completeGroup, status) = await _thingGroupService.getGroup (group.thingGroupId);
                 if (status == HttpStatusCode.OK)
-                    contains = completeGroup.things.Select(x => x.thingId).Contains(thingId);
+                    contains = completeGroup.things.Select (x => x.thingId).Contains (thingId);
             }
             if (!contains)
                 return (null, "This Tool can't  be associated with this thing.");
-            await _stateManagementService.setToolToStatusById(toolId, stateEnum.in_use, null);
-            await _toolService.setToolToThing(tool, thingId);
-            Trigger(tool);
-            tool = await _toolService.getTool(toolId);
+            await _stateManagementService.setToolToStatusById (toolId, stateEnum.in_use, null);
+            await _toolService.setToolToThing (tool, thingId);
+            Trigger (tool);
+            tool = await _toolService.getTool (toolId);
             return (tool, "Tool Set to Use");
         }
 
-        public async Task<(Tool, string)> DisassociateTool(Tool tool)
-        {
-            var toolDb = await _toolService.getTool(tool.toolId);
+        public async Task<(Tool, string)> DisassociateTool (Tool tool) {
+            var toolDb = await _toolService.getTool (tool.toolId);
             if (tool == null)
                 return (null, "Tool Not Found");
-            if (toolDb.status != stateEnum.in_use.ToString())
+            if (toolDb.status != stateEnum.in_use.ToString ())
                 return (null, "Tool Not In Use");
             if (toolDb.currentLife > tool.currentLife)
                 return (null, "Current Life can be greater than the previou Life");
-            await _stateManagementService.setToolToStatusById(tool.toolId, stateEnum.available, null);
-            await _toolService.setToolToThing(toolDb, null);
-            Trigger(tool);
-            var newtool = await _toolService.getTool(tool.toolId);
+            await _stateManagementService.setToolToStatusById (tool.toolId, stateEnum.available, null);
+            await _toolService.setToolToThing (toolDb, null);
+            Trigger (tool);
+            var newtool = await _toolService.getTool (tool.toolId);
             return (newtool, "Tool Set to Available");
 
         }
 
-        private async void Trigger(Tool tool)
-        {
-            if (_configuration["AssociationPostEndpoint"] != null)
-            {
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                var content = new StringContent(JsonConvert.SerializeObject(tool), Encoding.UTF8, "application/json");
-                HttpResponseMessage response = await client.PostAsync(_configuration["AssociationPostEndpoint"], content);
-                if (response.IsSuccessStatusCode)
-                {
-                    Console.WriteLine("Data posted on AssociationPostEndpoint");
+        private async void Trigger (Tool tool) {
+            if (_configuration["AssociationPostEndpoint"] != null) {
+                client.DefaultRequestHeaders.Accept.Clear ();
+                client.DefaultRequestHeaders.Accept.Add (new MediaTypeWithQualityHeaderValue ("application/json"));
+                var content = new StringContent (JsonConvert.SerializeObject (tool), Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync (_configuration["AssociationPostEndpoint"], content);
+                if (response.IsSuccessStatusCode) {
+                    Console.WriteLine ("Data posted on AssociationPostEndpoint");
                 }
             }
         }
